a95f74bac650b2733baa363b5135ba43a213dc69
Cynthia Mugo <cynthiamugo25@gmail.com>
2025-06-06 13:07:40 +0300

    if else statement

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (feet > 400 && feet <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (feet > 2000 && feet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
+  return result;
 }
+console.log(scuberGreetingForFeet(400));
+console.log(scuberGreetingForFeet(1500));
+console.log(scuberGreetingForFeet(2001));
+console.log(scuberGreetingForFeet(2501));
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  // if (city === 'NYC') {
+  //   return 'Ok, sounds good.';
+  // } else {
+  //   return 'No go.'
+  // }
 }
+console.log(ternaryCheckCity('NYC'));
+console.log(ternaryCheckCity('Columbia'));
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+    return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
+console.log(switchOnCharmFromTip('generous'));
+console.log(switchOnCharmFromTip('not as generous'));
+console.log(switchOnCharmFromTip('thanks for everything'));
